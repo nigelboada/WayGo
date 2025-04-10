f1148545dbc7d791a9d440fa2e550e14e5a1ed8e
nigel <nboadag@gmail.com>
2025-04-10 10:00:53 +0200

    Add splash screen layout and SplashActivity for app launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.testing)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
